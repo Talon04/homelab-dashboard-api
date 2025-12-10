b6c7586a271aba63a1e408ea1fb01a0d33e1553c
Talon <jannekschulz04@gmail>
2025-12-10 23:18:37 +0100

    smll thingy

--- backend/docker_utils.py
+++ backend/docker_utils.py
@@ -1,7 +1,6 @@
 import os
 import docker
-dontUseDocker = True
-#dontUseDocker = os.environ.get("DONT_USE_DOCKER", "false").lower() == "true"
+dontUseDocker = os.environ.get("DONT_USE_DOCKER", "false").lower() == "true"
 print(dontUseDocker)
 def list_containers():
     if dontUseDocker:
